9fd209ed41e569513245995d5a46b1dc128b92bb
Mark Goldstein <GoldsteinM@vpcc.edu>
2025-02-07 11:02:14 -0500

    Add project files.

---
+++ Chapter2Exerise17.cpp
@@ -0,0 +1,25 @@
+/*
+File Name:Chapter2Exercise17.cpp
+Developer: Mark Goldstein
+Date:Feb 2025;
+Requirements;
+Energy Drink Consumption  A soft drink company 
+recently surveyed 16,500 of its customers and found 
+that approximately 15 percent of those surveyed 
+purchase one or more energy drinks per week. Of those 
+customers who purchase energy drinks, approximately 58 
+percent of them prefer citrus-flavored energy drinks. 
+Write a program that displays the following: 
+• The approximate number of customers in the survey who 
+purchase one or more energy drinks per week.• The approximate 
+number of customers 
+in the survey who prefer citrus-flavored energy drinks.
+*/
+#include <iostream>
+#include <iomanip>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
